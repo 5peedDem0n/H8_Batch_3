304097f81fc53f11277265edae55deec90085809
5peedDem0n <felistigris13@gmail.com>
2022-08-25 15:41:22 +0700

    Initial Commit add Sesi 1 & Sesi 2

---
+++ sesi_2/PythonSimpleGame.py
@@ -0,0 +1,21 @@
+import random
+
+number = [0,1,2,3,4,5,6,7,8,9]
+com = random.choice(number)
+me = 10
+trying = 0
+while me != com:
+    me = input('Pick any number!\n')
+    me = int(me)
+    if me > com:
+        print("Pilihan anda terlalu besar!")
+    elif me < com:
+        print("Pilihan anda terlalu kecil")
+    else:
+        print("Tebakan anda benar")
+    trying += 1
+    print(f"Percobaan {trying} gagal!")
+    if trying == 3:
+        print("GAME OVER")
+        break
+    
